5de0afc159d1821b38057b1b4cc53141b8042b78
Ariel Wolfmann <awolfmann@gmail.com>
2015-09-15 12:13:09 +0300

    starting with some ideas for exercises 2 and 3

--- languagemodeling/ngram.py
+++ languagemodeling/ngram.py
@@ -1,5 +1,6 @@
 # https://docs.python.org/3/library/collections.html
 from collections import defaultdict
+import math
 
 
 class NGram(object):
@@ -27,3 +28,77 @@ class NGram(object):
 
         tokens = prev_tokens + [token]
         return float(self.counts[tuple(tokens)]) / self.counts[tuple(prev_tokens)]
+ 
+    def count(self, tokens):
+        """Count for an n-gram or (n-1)-gram.
+ 
+        tokens -- the n-gram or (n-1)-gram tuple.
+        """
+ 
+    def cond_prob(self, token, prev_tokens=None):
+        """Conditional probability of a token.
+ 
+        token -- the token.
+        prev_tokens -- the previous n-1 tokens (optional only if n = 1).
+        """
+        n = self.n
+        if not prev_tokens:
+            prev_tokens = []
+        assert len(prev_tokens) == n - 1
+
+        tokens = prev_tokens + [token]
+        return float(self.counts[tuple(tokens)]) / self.counts[tuple(prev_tokens)]
+
+    def sent_prob(self, sent):
+        """Probability of a sentence. Warning: subject to underflow problems.
+ 
+        sent -- the sentence as a list of tokens.
+        """
+        sent_prob = 1.0
+        for i in range(len(sent)):
+            sent_prob *= self.cond_prob(sent[i], sent[i-self.n:i])
+        # VER CASOS i<n
+        return sent_prob
+
+    def sent_log_prob(self, sent):
+        """Log-probability of a sentence.
+ 
+        sent -- the sentence as a list of tokens.
+        """
+        sent_log_prob = 1.0
+        for i in range(len(sent)):
+            sent_log_prob *= math.log(self.cond_prob(sent[i], sent[i-self.n:i]))
+        # VER CASOS i<n
+        return sent_log_prob
+
+
+class NGramGenerator:
+ 
+    def __init__(self, model):
+        """
+        model -- n-gram model.
+        """
+        self.model = model
+
+    def generate_sent(self):
+        """Randomly generate a sentence."""
+        sent = []
+        prev_tokens = []
+        i = 0
+        while token != "STOP":
+            prev_tokens = sent[i - self.model.n : i]
+            token = self.generate_token(prev_tokens)
+            sent.append(token)
+            i += 1
+
+        return sent
+
+    def generate_token(self, prev_tokens=None):
+        """
+        Randomly generate a token, given prev_tokens.
+ 
+        prev_tokens -- the previous n-1 tokens (optional only if n = 1).
+        """
+
+        # calcular cual de todas las palabras del vocabulario es la 
+        # mas probable que ocurra dado prev_tokens? 
---
+++ languagemodeling/scripts/generate.py
@@ -0,0 +1,34 @@
+"""Generate natural language sentences using a language model.
+
+Usage:
+  generate.py -i <file> -n <n>
+  generate.py -h | --help
+
+Options:
+  -i <file>     Language model file.
+  -n <n>        Number of sentences to generate.
+  -h --help     Show this screen.
+"""
+from docopt import docopt
+import pickle
+
+from nltk.corpus import gutenberg
+
+from languagemodeling.ngram import NGramGenerator
+
+
+if __name__ == '__main__':
+    opts = docopt(__doc__)
+
+    # load the model
+    filename = opts['-i']
+    pkl_file = open(filename, 'rb')
+    model = pickle.load(pkl_file)
+    pkl_file.close()
+
+    # generate sentences
+    generator = NGramGenerator(model)
+    n = int(opts['-n'])
+    for i in range(n):
+      sent = generator.generate_sent()
+      print sent
