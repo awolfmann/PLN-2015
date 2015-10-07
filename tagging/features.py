7e2d2a665336b5899bba497ba2d34667a4bc60e5
Ariel Wolfmann <awolfmann@gmail.com>
2015-10-07 12:41:43 +0300

    advances in ex 6

--- tagging/features.py
+++ tagging/features.py
@@ -8,6 +8,7 @@ from featureforge.feature import Feature
 # i -- the position to be tagged.
 History = namedtuple('History', 'sent prev_tags i')
 
+# una vez que el tagger calculo el tag para la primer palabra
 
 def word_lower(h):
     """Feature: current lowercased word.
@@ -17,6 +18,28 @@ def word_lower(h):
     sent, i = h.sent, h.i
     return sent[i].lower()
 
+def word_istitle(h):
+    """Feature: current lowercased word.
+    h -- a history.
+    """
+    sent, i = h.sent, h.i
+    return sent[i].istitle()
+
+def word_isupper(h):
+    """Feature: current lowercased word.
+    h -- a history.
+    """
+    sent, i = h.sent, h.i
+    return sent[i].isupper()
+
+def word_isdigit(h):
+    """Feature: current lowercased word.
+    h -- a history.
+    """
+    sent, i = h.sent, h.i
+    return sent[i].isdigit()
+
+
 class NPrevTags(Feature):
  
     def __init__(self, n):
@@ -24,13 +47,17 @@ class NPrevTags(Feature):
  
         n -- number of previous tags to consider.
         """
+        self.n = n
  
     def _evaluate(self, h):
         """n previous tags tuple.
  
         h -- a history.
         """
- 
+        sent, i, h.prev_tags = h.sent, h.i, h.prev_tags
+        if len()
+        return h.prev_tags[:]
+         
  
 class PrevWord(Feature):
  
@@ -39,9 +66,12 @@ class PrevWord(Feature):
  
         f -- the feature.
         """
+        self.f = f
  
     def _evaluate(self, h):
         """Apply the feature to the previous word in the history.
  
         h -- the history.
         """
+        history = History(h.sent, h.prev_tags, h.i-1)
+        return self.f(history)
