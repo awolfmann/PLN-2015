d36d5e1f19cded85dbc7c8e0627f8a7ed2837c45
Ariel Wolfmann <awolfmann@gmail.com>
2015-10-17 12:53:59 +0300

    adding hmm as posible tagger

--- tagging/scripts/eval.py
+++ tagging/scripts/eval.py
@@ -38,23 +38,37 @@ if __name__ == '__main__':
     sents = list(corpus.tagged_sents())
 
     # tag
-    hits, total = 0, 0
+    hits, total, hits_unk, total_unk = 0, 0, 0, 0
     n = len(sents)
-    for i, sent in enumerate(sents):
+    for i, sent in enumerate(filter(lambda x: x, sents)):
         word_sent, gold_tag_sent = zip(*sent)
-
         model_tag_sent = model.tag(word_sent)
         assert len(model_tag_sent) == len(gold_tag_sent), i
 
         # global score
         hits_sent = [m == g for m, g in zip(model_tag_sent, gold_tag_sent)]
         hits += sum(hits_sent)
-        total += len(sent)
+        total += len(sent) 
         acc = float(hits) / total
 
+        unk_words_tags = [item for item in sent if model.unknown(item[0])]
+        unk_words = [item[0] for item in unk_words_tags]
+        model_tag_unk = model.tag(unk_words)
+        gold_tag_unk = [item[1] for item in unk_words_tags]
+        hits_sent_unk = [m == g for m, g in zip(model_tag_unk, gold_tag_unk)]
+        hits_unk += sum(hits_sent_unk)
+        total_unk += len(unk_words) 
         progress('{:3.1f}% ({:2.2f}%)'.format(float(i) * 100 / n, acc * 100))
 
     acc = float(hits) / total
+    acc_unk = float(hits_unk) / total_unk
+    acc_kno = float(hits - hits_unk) / (total - total_unk)
 
     print('')
     print('Accuracy: {:2.2f}%'.format(acc * 100))
+    print('Accuracy Unknown: {:2.2f}%'.format(acc_unk * 100))
+    print('Accuracy known: {:2.2f}%'.format(acc_kno * 100))
+    print('hits', hits)
+    print('total', total)
+    print('hits_unk', hits_unk)
+    print('total_unk', total_unk)
--- tagging/scripts/train.py
+++ tagging/scripts/train.py
@@ -1,24 +1,31 @@
 """Train a sequence tagger.
 
 Usage:
-  train.py [-m <model>] -o <file>
+  train.py [-m <model>] -o <file> [-n <n>] 
   train.py -h | --help
 
 Options:
   -m <model>    Model to use [default: base]:
                   base: Baseline
+                  hmm: ML hmm
+                  memm: MEMM
   -o <file>     Output model file.
   -h --help     Show this screen.
+  -n            Order of the model
 """
 from docopt import docopt
 import pickle
 
 from corpus.ancora import SimpleAncoraCorpusReader
 from tagging.baseline import BaselineTagger
+from tagging.hmm import HMM, MLHMM
+from tagging.memm import MEMM
 
 
 models = {
     'base': BaselineTagger,
+    'hmm' : MLHMM,
+    'memm' : MEMM, 
 }
 
 
@@ -31,7 +38,12 @@ if __name__ == '__main__':
     sents = list(corpus.tagged_sents())
 
     # train the model
-    model = models[opts['-m']](sents)
+    model = None
+    if opts['-m'] != 'base':
+        n = int(opts['-n'])
+        model = models[opts['-m']](n, sents)
+    else:
+        model = models[opts['-m']](sents)
 
     # save it
     filename = opts['-o']
