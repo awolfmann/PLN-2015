9328d9edb81f98aa7ce832132053f509a7bafe89
Ariel Wolfmann <awolfmann@gmail.com>
2015-10-30 12:44:43 +0200

    starting with upcfg

--- parsing/upcfg.py
+++ parsing/upcfg.py
@@ -1,3 +1,7 @@
+from collections import defaultdict
+from nltk.tree import Tree
+from nltk.grammar import PCFG, induce_pcfg, Nonterminal
+
 class UPCFG(object):
     """Unlexicalized PCFG.
     """
@@ -6,10 +10,25 @@ class UPCFG(object):
         """
         parsed_sents -- list of training trees.
         """
- 
+        # assert is binarised
+        # induce pcfg induce_pcfg(start, productions)
+        self.parsed_sents = parsed_sents
+        self.productions = []
+        starters = defaultdict(int)
+        for tree in parsed_sents:
+            starters[tree.label()] += 1
+            self.productions += list(tree.productions())
+
+        start = max(starters, key=starters.get)
+        start = Nonterminal(start)
+
+        self.pcfg = induce_pcfg(start, self.productions)
+        assert self.pcfg.is_binarised()
+
     def productions(self):
         """Returns the list of UPCFG probabilistic productions.
         """
+
  
     def parse(self, tagged_sent):
         """Parse a tagged sentence.
