c5eac1f5f0f751531c0449ea16d75ae733188e11
Ariel Wolfmann <awolfmann@gmail.com>
2016-12-14 14:58:17 -0300

    Designation unique entity ocurrence

--- information_extraction/resoluciones-unc/process/designation.py
+++ information_extraction/resoluciones-unc/process/designation.py
@@ -11,7 +11,18 @@ from process.regexp_ner import (RegExpNERRunner, options_re, optional_re,
 class DesignationNERRunner(RegExpNERRunner):
 
     def __init__(self, override=False):
+        self.count = 0
         designations = ['Deslgnar', 'Aceptar', 'Prorrogar', 'designaci6n', 'Otorgar', 'Designar']
         designations_p = ['designaciones', ]
         regexp = options_re(designations+designations_p)
         super(DesignationNERRunner, self).__init__('designation', regexp, override)
+
+    def process_match(self, match):
+        # name = ' '.join(match.group())
+        name = 'designacion {}'.format(self.count)
+        self.count += 1
+        kind = self.label
+        offset, offset_end = match.span()
+        entity_oc = self.build_occurrence(name, kind, name, offset, offset_end)
+
+        return entity_oc
