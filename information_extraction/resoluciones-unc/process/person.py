afca421ed1862e813aeae710cb2bae7fcb34a130
Ariel Wolfmann <awolfmann@gmail.com>
2016-12-14 14:57:33 -0300

    some fixes in Person NER

--- information_extraction/resoluciones-unc/process/person.py
+++ information_extraction/resoluciones-unc/process/person.py
@@ -15,16 +15,17 @@ class PersonNERRunner(RegExpNERRunner):
         # surname_re = upperletters_re('surname')
         # surname_re = u'(?P<<surname>><[A-ZÁÉÍÓÚÑ]*>)'
         # surnames_re = u'(?P<<surname>><{}*\s{}*>)'.format(upperletter, upperletter)
-        surname = upperletters_re() + optional_re(upperletters_re())
+        surname = u'<[A-ZÁÉÍÓÚÑ][A-ZÁÉÍÓÚÑ]+>+'
         # VER CASO MOISSET DE ESPANES
         # Ver Diego DUBOIS, Demetrio VILELA y Carlos BEDERIAN
         # name_re = lowerletters_re('name')
         # name_re = u'(?P<<name>><[A-ZÁÉÍÓÚÑ][a-záéíóúñ]*>)'
-        name = capitalizedletters_re() + optional_re(capitalizedletters_re()) 
-        surname_name = surname + tokenized_re('<,>') + name
-        # name_surname = name + optional_re(tokenized_re('<.>')) + surname # Caso: Daniel  MOISSET  DE  ESPANES  
-        # regexp = '(' + surname_name + ' | ' + name_surname + ')'
-        regexp = surname_name
+        name = '<[A-ZÁÉÍÓÚÑ][a-záéíóúñ]+>+'
+        surname_name = surname + '<,>' + name
+        name_surname = name + '(<[A-ZÁÉÍÓÚÑ]><.>)?' + surname 
+        # Caso: Daniel  MOISSET  DE  ESPANES  
+        regexp = '(' + surname_name + ' | ' + name_surname + ')'
+        # regexp = surname_name
         super(PersonNERRunner, self).__init__('person', regexp, override)
 
     # def process_match(self, match):
